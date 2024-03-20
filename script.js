a09a111470c8e68770a9d22384ae2ce3b446dc50
Cerenar <camerondonner63@gmail.com>
2024-03-20 16:08:47 -0500

    Add styling to finish MVP
    This small drawing toy now includes cell coloring, cell erasure,
    grid erasure, grid resizing, and color picking all with
    a clean user interface. Grid lines cannot be disabled, and there
    is no color randomization yet, but this is a good minimum
    viable product.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridContainer = document.querySelector("#container");
 const colorPicker = document.querySelector("#colorPicker");
 const gridSizer = document.querySelector("#gridSizer");
+const gridEraser = document.querySelector("#gridEraser");
+const CONTAINER_SIZE = 700;
 let isMouseDown = false;
 let isRightMouseDown = false;
 let gridSize = 16;
@@ -16,19 +18,27 @@ gridContainer.addEventListener("contextmenu", (e) => {
 
 gridSizer.addEventListener("click", () => {
     gridSize = prompt("Grid size?");
+    if(gridSize === null) return;
     while (gridSize < 4 || gridSize > 100) {
+        if (gridSize === null) {
+            return;
+        }
         gridSize = prompt("Please select a grid size between 4 and 100.")
     }
     createGrid(gridSize);
 });
+
+gridEraser.addEventListener("click", () => {
+    eraseGrid();
+})
 
 function createGrid(gridSize) {
     removeGrid();
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             const gridSquare = document.createElement("div");
-            gridSquare.style.height = 700/gridSize + "px";
-            gridSquare.style.width = 700/gridSize + "px";
+            gridSquare.style.height = CONTAINER_SIZE/gridSize + "px";
+            gridSquare.style.width = CONTAINER_SIZE/gridSize + "px";
             gridContainer.appendChild(gridSquare);
         }
     }
@@ -58,6 +68,13 @@ function createGrid(gridSize) {
             isMouseDown = false;
             isRightMouseDown = false;
         });
+    }
+}
+
+function eraseGrid() {
+    const gridSquares = document.querySelectorAll("#container div");
+    for (const gridSquare of gridSquares) {
+        gridSquare.style["background-color"] = null;
     }
 }
 
