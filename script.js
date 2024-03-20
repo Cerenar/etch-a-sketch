50ea4f9e7b78710a3da8dc16efc47ac05f340792
Cerenar <camerondonner63@gmail.com>
2024-03-20 15:18:43 -0500

    Add grid resizing

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const gridContainer = document.querySelector("#container");
 const colorPicker = document.querySelector("#colorPicker");
+const gridSizer = document.querySelector("#gridSizer");
 let isMouseDown = false;
 let isRightMouseDown = false;
+let gridSize = 16;
+createGrid(gridSize);
 
 gridContainer.addEventListener("dragstart", (e) => {
     e.preventDefault();
@@ -11,38 +14,60 @@ gridContainer.addEventListener("contextmenu", (e) => {
     e.preventDefault();
 })
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const gridSquare = document.createElement("div");
-        gridContainer.appendChild(gridSquare);
+gridSizer.addEventListener("click", () => {
+    gridSize = prompt("Grid size?");
+    while (gridSize < 4 || gridSize > 100) {
+        gridSize = prompt("Please select a grid size between 4 and 100.")
+    }
+    createGrid(gridSize);
+});
+
+function createGrid(gridSize) {
+    removeGrid();
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.style.height = 700/gridSize + "px";
+            gridSquare.style.width = 700/gridSize + "px";
+            gridContainer.appendChild(gridSquare);
+        }
+    }
+
+    const gridSquares = document.querySelectorAll("#container div");
+    for (const gridSquare of gridSquares) {
+        gridSquare.addEventListener("mousedown", (e) => {
+            if (e.button === 0) {
+                gridSquare.style.backgroundColor = colorPicker.value;
+                isMouseDown = true;
+            }
+            else if (e.button === 2) {
+                gridSquare.style["background-color"] = null;
+                isRightMouseDown = true;
+            }
+            
+        });
+        gridSquare.addEventListener("mouseover", () => {
+            if (isMouseDown) {
+                gridSquare.style.backgroundColor = colorPicker.value;
+            }
+            if (isRightMouseDown) {
+                gridSquare.style["background-color"] = null;
+            }
+        });
+        gridSquare.addEventListener("mouseup", () => {
+            isMouseDown = false;
+            isRightMouseDown = false;
+        });
     }
 }
 
-let gridSquares = document.querySelectorAll("#container div");
-
-for (const gridSquare of gridSquares) {
-    gridSquare.addEventListener("mousedown", (e) => {
-        if (e.button === 0) {
-            gridSquare.style.backgroundColor = colorPicker.value;
-            isMouseDown = true;
-        }
-        else if (e.button === 2) {
-            gridSquare.style.backgroundColor = "#ffffff";
-            isRightMouseDown = true;
-        }
-        
-    });
-    gridSquare.addEventListener("mouseover", () => {
-        if (isMouseDown) {
-            gridSquare.style.backgroundColor = colorPicker.value;
-        }
-        if (isRightMouseDown) {
-            gridSquare.style.backgroundColor = "#ffffff";
-        }
-    });
-    gridSquare.addEventListener("mouseup", () => {
-        isMouseDown = false;
-        isRightMouseDown = false;
-    });
+function removeGrid() {
+    while (gridContainer.children.length > 0) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
 }
+
+
+
+
 
